c4f6286e6ece4032b7d5cf99587dfb9fc2420e50
Aneracam77 <aneracam77@hotmail.com>
2023-03-29 18:52:34 +0200

    Proyecto a las 21

---
+++ src/com/hedima/presentacion/ProbarClaseString2.java
@@ -0,0 +1,12 @@
+package com.hedima.presentacion;
+
+public class ProbarClaseString2 {
+    public static void main(String[] args) {
+        char letra = 'a';
+        String nombre = "Juan";
+        String apellido = "Perez";
+        System.out.println(nombre+ " "+apellido);
+        System.out.println(nombre.concat(" "+apellido));
+    }
+
+}
